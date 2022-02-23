0716c6987ff608a2f6b088a13e3ca6666749a82e
bravo-96 <67931914+bravo-96@users.noreply.github.com>
2022-02-22 23:27:33 -0300

    se configuro la minificacion de babel en el build

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,81 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+   mode: 'production',
 
-  entry: {
-    app: './src/index.js',
-  },
+   entry: {
+      app: './src/index.js',
+   },
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    clean: true,
-    assetModuleFilename: 'static/[hash][ext]',
-  },
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.[contenthash].js',
+      clean: true,
+      assetModuleFilename: 'static/[hash][ext]',
+   },
 
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: false },
-          },
-        ],
+   module: {
+      rules: [
+         {
+            test: /\.html$/,
+            use: [
+               {
+                  loader: 'html-loader',
+                  options: { minimize: false },
+               },
+            ],
+         },
+         {
+            test: /\.js/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+         },
+         {
+            test: /\.(css|scss|sass)$/,
+            use: [
+               MiniCssExtractPlugin.loader,
+               'css-loader',
+               'sass-loader',
+               'postcss-loader',
+            ],
+         },
+         {
+            test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+            type: 'asset/resource',
+            use: ['image-webpack-loader'],
+         },
+      ],
+   },
+
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: 'src/index.html',
+         filename: 'index.html',
+      }),
+      new MiniCssExtractPlugin({
+         filename: 'main.[contenthash].css',
+      }),
+      new MinifyPlugin(),
+   ],
+
+   optimization: {
+      minimizer: [new CssMinimizerPlugin()],
+   },
+
+   devServer: {
+      open: true,
+      port: 4040,
+      compress: true,
+      client: {
+         overlay: {
+            errors: true,
+            warnings: false,
+         },
       },
-      {
-        test: /\.js/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(css|scss|sass)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-          'postcss-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-        type: 'asset/resource',
-        use: ['image-webpack-loader'],
-      },
-    ],
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'main.css',
-    }),
-  ],
-
-  optimization: {
-    minimizer: [new CssMinimizerPlugin()],
-  },
-
-  devServer: {
-    open: true,
-    port: 4040,
-    compress: true,
-    client: {
-      overlay: {
-        errors: true,
-        warnings: false,
-      },
-    },
-  },
+   },
 };
